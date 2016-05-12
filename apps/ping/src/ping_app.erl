b02ff5f3f1a3e93df18b8d75067eab3f3e7b02ef
sescobb27 <s.escob.b.27@gmail.com>
2016-05-11 22:48:33 -0500

    [ping] ping OTP app working

--- apps/ping/src/ping_app.erl
+++ apps/ping/src/ping_app.erl
@@ -8,11 +8,14 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1]).
+-export([start/2, start/0, stop/1]).
 
 %%====================================================================
 %% API
 %%====================================================================
+-spec start() -> {ok, [atom()]} | {error, term()}.
+start() ->
+  {ok, _} = application:ensure_all_started(ping).
 
 start(_StartType, _StartArgs) ->
     Dispatch = cowboy_router:compile([
@@ -20,8 +23,8 @@ start(_StartType, _StartArgs) ->
         {"/ping", ping_handler, []}
       ]}
     ]),
-    {ok, _} = cowboy:start_http(ec_api, 100,
-      [{port, 3000}],
+    {ok, _} = cowboy:start_http(ping, 100,
+      [{port, 4000}],
       [{env, [{dispatch, Dispatch}]}]
     ),
     ping_sup:start_link().
--- apps/ping/src/ping_handler.erl
+++ apps/ping/src/ping_handler.erl
@@ -1,11 +1,28 @@
 -module (ping_handler).
 -export ([init/3, terminate/3, handle/2]).
 
-init(_Type, Req, []) ->
-  {shutdown, Req, no_state}.
+-record (state, {
+  pong_channel :: pid(),
+  ping_channel :: pid()
+}).
 
-handle(Req, Opts) ->
-  {ok, Req, Opts}.
+init(_Type, Req, []) ->
+  PongChannel = bunny_client:init(<<"pong">>),
+  PingChannel = bunny_client:init(<<"ping">>),
+  State = #state{
+    pong_channel = PongChannel,
+    ping_channel = PingChannel
+  },
+  {ok, Req, State}.
+
+handle(Req, State) ->
+  io:format("[producing] PING_MESSAGE to ping queue~n"),
+  bunny_client:produce(State#state.ping_channel, <<"ping">>, <<"PING_MESSAGE">>),
+  bunny_client:subscribe(State#state.pong_channel, <<"pong">>, self()),
+  bunny_client:consume_msg(State#state.pong_channel, <<"pong">>),
+  bunny_client:close(State#state.ping_channel),
+  bunny_client:close(State#state.pong_channel),
+  {ok, Req, State}.
 
 terminate(_Reason, _Req, _State) ->
   ok.
