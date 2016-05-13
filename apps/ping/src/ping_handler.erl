1ac65f4483a87d66ea854f86caf5e4b4e73f8176
sescobb27 <s.escob.b.27@gmail.com>
2016-05-13 01:35:09 -0500

    [refactor] ping OTP app working with poolboy

--- apps/ping/src/ping_handler.erl
+++ apps/ping/src/ping_handler.erl
@@ -1,27 +1,19 @@
 -module (ping_handler).
 -export ([init/3, terminate/3, handle/2]).
 
--record (state, {
-  pong_channel :: pid(),
-  ping_channel :: pid()
-}).
-
 init(_Type, Req, []) ->
-  PongChannel = bunny_client:init(<<"pong">>),
-  PingChannel = bunny_client:init(<<"ping">>),
-  State = #state{
-    pong_channel = PongChannel,
-    ping_channel = PingChannel
-  },
-  {ok, Req, State}.
+  {ok, Req, []}.
 
 handle(Req, State) ->
-  io:format("[producing] PING_MESSAGE to ping queue~n"),
-  bunny_client:produce(State#state.ping_channel, <<"ping">>, <<"PING_MESSAGE">>),
-  bunny_client:subscribe(State#state.pong_channel, <<"pong">>, self()),
-  bunny_client:consume_msg(State#state.pong_channel, <<"pong">>),
-  bunny_client:close(State#state.ping_channel),
-  bunny_client:close(State#state.pong_channel),
+  poolboy:transaction(bunny_pool, fun (Worker) ->
+    io:format("[producing] PING_MESSAGE to ping queue~n"),
+    Connection = gen_server:call(Worker, get_connection),
+    Channel = bunny_client:open_channel(Connection),
+    bunny_client:produce(Channel, <<"ping">>, <<"PING_MESSAGE">>),
+    bunny_client:subscribe(Channel, <<"pong">>, self()),
+    bunny_client:consume_msg(Channel, <<"pong">>),
+    bunny_client:close_channel(Channel)
+  end, infinity),
   {ok, Req, State}.
 
 terminate(_Reason, _Req, _State) ->
