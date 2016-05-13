c88302efab36a0fe86236b4474925e463d3aea06
sescobb27 <s.escob.b.27@gmail.com>
2016-05-13 01:36:02 -0500

    [refactor] ping_pong OTP app

--- apps/ping_pong/src/ping_pong_app.erl
+++ apps/ping_pong/src/ping_pong_app.erl
@@ -8,15 +8,19 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1]).
+-export([start/2, start/0, stop/1]).
 
 %%====================================================================
 %% API
 %%====================================================================
+start() ->
+  application:start(?MODULE),
+  bunny_pool_sup:start_link(),
+  bunny_client:init(),
+  application:ensure_all_started(ping),
+  application:ensure_all_started(pong).
 
 start(_StartType, _StartArgs) ->
-    application:ensure_all_started(ping),
-    application:ensure_all_started(pong),
     ping_pong_sup:start_link().
 
 %%--------------------------------------------------------------------
