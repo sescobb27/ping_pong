c7ee4f306d1fc09729360db8a561b09a23a5e8f9
sescobb27 <s.escob.b.27@gmail.com>
2016-06-12 16:16:03 -0500

    [refactor] delete 2sec sleep [refactot] split process creation from process setup so we can restart died process easily

--- apps/pong/src/consumer.erl
+++ apps/pong/src/consumer.erl
@@ -1,18 +1,20 @@
 -module (consumer).
 
--export ([consume_with_reply/2, loop/2]).
+-export ([consume_with_reply/2, loop/2, create_consumers/3]).
 
 consume_with_reply(Connection, Queue) ->
+  Channel = bunny_client:open_channel(Connection),
+  Pid = spawn_link(?MODULE, loop, [Channel, Queue]),
+  bunny_client:subscribe(Channel, Queue, Pid),
+  Pid.
+
+create_consumers(N, Connection, Queue) ->
   lists:map(fun(_) ->
-    Channel = bunny_client:open_channel(Connection),
-    Pid = spawn(?MODULE, loop, [Channel, Queue]),
-    bunny_client:subscribe(Channel, Queue, Pid),
-    Pid
-  end, lists:seq(0, 255)).
+    consume_with_reply(Connection, Queue)
+  end, lists:seq(0, N)).
 
 loop(Channel, Queue) ->
   bunny_client:consume_msg(Channel, Queue),
-  timer:sleep(2000),
   db:increment(),
   io:format("[producing] PONG_MESSAGE to pong queue~n"),
   bunny_client:produce(Channel, <<"pong">>, <<"PONG_MESSAGE">>),
