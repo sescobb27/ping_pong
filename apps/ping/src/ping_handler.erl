3fb3849c088b521e7fc6ae3ad28f3cdaae37198d
sescobb27 <s.escob.b.27@gmail.com>
2016-10-08 21:11:39 -0500

    [refactor] retrial ping

--- apps/ping/src/ping_handler.erl
+++ apps/ping/src/ping_handler.erl
@@ -5,16 +5,41 @@ init(_Type, Req, []) ->
   {ok, Req, []}.
 
 handle(Req, State) ->
+  retrial_ping(0),
+  {ok, Req, State}.
+
+retrial_ping(Times) ->
+  retrial_ping(0, Times).
+
+retrial_ping(Cont, Times) ->
+  try
+    exec_ping()
+  catch
+    exit:{timeout, Rest} ->
+      case Cont < Times of
+        true ->
+          retrial_ping(Cont + 1, Times);
+        _ ->
+          erlang:exit({timeout, Rest})
+      end
+  end.
+
+exec_ping() ->
   poolboy:transaction(bunny_ping_pool, fun (Worker) ->
     io:format("[producing] PING_MESSAGE to ping queue~n"),
     Connection = bunny_worker:get_connection(Worker),
     Channel = bunny_client:open_channel(Connection),
-    bunny_client:produce(Channel, <<"ping">>, <<"PING_MESSAGE">>),
-    bunny_client:subscribe(Channel, <<"pong">>, self()),
-    bunny_client:consume_msg(Channel, <<"pong">>),
+    Key = make_ref(),
+    Msg = #{
+      msg => <<"PING_MESSAGE">>,
+      key => Key
+    },
+    BinaryMsg = term_to_binary(Msg),
+    bunny_client:produce(Channel, <<"ping">>, BinaryMsg),
+    bunny_client:subscribe(Channel, <<"pong">>),
+    bunny_client:consume_msg(Channel, <<"pong">>, Key),
     bunny_client:close_channel(Channel)
-  end, infinity),
-  {ok, Req, State}.
+  end).
 
 terminate(_Reason, _Req, _State) ->
   ok.
